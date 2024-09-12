5357d23e18b47dcc4207f738a0962a3769deb7cd
zero-day <156213823+leilvm@users.noreply.github.com>
2024-09-12 18:50:41 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,25 @@ from datetime import datetime
 
 
 class BaseModel:
-    """
-    A base model class to inherit common functionalities.
-    """
-    
-    def __init__(self):
-        self.name = "BaseModel"
-    
-    def print_name(self):
-        print(self.name)
+"""
+class BaseModel
+"""
+    def __init__(self, *args, **kwargs): """initialize new basemodel instance arguments
+    *args
+    **kwargs"""
 
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) !=0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+            else:
+                models.storage.new(self)
     def save(self):
                 self.updated_at = datetime.today()
                 models.storage.save()
